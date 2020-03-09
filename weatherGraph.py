6462da69f4178d92230a5cfd6a9bf79d4ececaf3
meatbballa <42406133+meatbballa@users.noreply.github.com>
2020-03-09 08:16:49 -1000

    Add files via upload

---
+++ weatherGraph.py
@@ -0,0 +1,27 @@
+import time
+import MySQLdb
+
+import Adafruit_DHT
+#Open database connection
+db =  MySQLdb.connect (host="localhost",user="root",passwd="raspberry",db="stem")
+#prep a cursor object using cursor() method; cursor is like a navigation method
+db.autocommit(True)
+cursor = db.cursor(MySQLdb.cursors.DictCursor)
+# Sensor should be set to Adafruit_DHT.DHT11,
+# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+sensor = Adafruit_DHT.DHT11
+
+pin = 18
+
+
+# Try to grab a sensor reading.  Use the read_retry method which will retry up
+# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+while humidity !=0 and temperature !=0:
+	sql = f"INSERT INTO humidity (temperature, humidity) VALUES ({temperature}, {humidity})"
+	cursor.execute(sql) 
+	time.sleep(5)
+
+cursor.close()
+db.close()
